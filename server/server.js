c545fd42ac61a29a8830d48558a4d135428c2580
Affaq1234 <affaqahmed2002@gmail.com>
2025-09-06 15:06:22 +0500

    Fix Build Problem 3

--- server/server.js
+++ server/server.js
@@ -4,14 +4,13 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
+const app = express();
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
-
-dotenv.config();
-const app = express();
 
 app.use(cors());
 app.use(express.json());
